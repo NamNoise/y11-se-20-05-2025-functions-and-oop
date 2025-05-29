9d2c7a2478f37e08c358b860a9c38106fb187c92
NamNoise <nam.hoang916@education.nsw.gov.au>
2025-05-29 11:50:57 +1000

    Create turtlerace.py

---
+++ turtlerace.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+leonardo = Turtle()
+leonardo.color('blue')
+leonardo.shape('turtle')
+leonardo.penup()
+leonardo.goto(-160, 100)
+leonardo.pendown()
+
+michelangelo = Turtle()
+donatello = Turtle()
+raphael = Turtle()
+
+michelangelo.penup()
+michelangelo.goto(-160, 70)
+michelangelo.pendown()
